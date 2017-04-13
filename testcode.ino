c5545f3b1d6b9ed5e137aee2d37c4a9590b9844c
Ian F <ijfranco@gmail.com>
2017-04-13 13:52:46 -0500

    addition of sketch file to master

---
+++ testcode.ino
@@ -0,0 +1,64 @@
+#include "SR04.h"
+#define TRIG_PIN 12
+#define ECHO_PIN 11
+
+//Global Variables
+SR04 sensor = SR04(ECHO_PIN,TRIG_PIN);
+const long MAX_DISTANCE = 40;
+const long DISTANCE_BUFFER = 3;
+long oldDistance;
+int lastEvent;
+int d = 500;
+
+
+void setup() {
+   Serial.begin(9600);
+   delay(1000);
+
+   oldDistance = 999;
+   lastEvent = 3;
+}
+
+
+void loop() {
+    long newDistance = sensor.Distance();
+    int event = 0;
+
+    if(lastEvent == 3) {
+      if(newDistance < MAX_DISTANCE) {
+        event = 1;
+        lastEvent = 1;
+        oldDistance = newDistance;
+      }
+      else {
+        oldDistance = newDistance;
+      }
+    }
+    else {
+      if(newDistance > MAX_DISTANCE){
+        event = 3;
+        lastEvent = 3;
+        oldDistance = newDistance;
+      }
+      else if(newDistance > oldDistance) {
+        event = 2;
+        lastEvent = 2;
+        oldDistance = newDistance;
+      }
+    }
+
+    //output data
+    if(event > 0) {
+      Serial.print("NEW: ");
+      Serial.print(newDistance);
+      Serial.print(" OLD: ");
+      Serial.print(oldDistance);
+      Serial.print(" EVENT: ");
+      Serial.print(event);
+      Serial.print("\n");
+
+      lastEvent = event;
+    }
+
+    delay(d);
+}
